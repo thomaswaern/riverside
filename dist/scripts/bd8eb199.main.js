eb77d4ab67898f36dab3fe7437d9bde88a6a97da
thomaswaern <thomas.waern@gmail.com>
2014-06-03 09:19:08 +0200

    Ported from meteor to webapp

--- dist/scripts/bd8eb199.main.js
+++
@@ -1,1 +0,0 @@
-"use strict";$(function(){$("a[href*=#]:not([href=#])").click(function(){if(location.pathname.replace(/^\//,"")===this.pathname.replace(/^\//,"")&&location.hostname===this.hostname){var a=$(this.hash);if(a=a.length?a:$("[name="+this.hash.slice(1)+"]"),a.length)return $("html,body").animate({scrollTop:a.offset().top},500),!1}})});
